d651924fdf4ecaaef8184102ce15d90e44b7d893
Gennadiy <renardes@gmail.com>
2023-04-09 15:14:32 +0500

    операторы: проверки на идентичность, математические, присвоения выражения

---
+++ base/operator_is.py
@@ -0,0 +1,15 @@
+int1 = 202
+int2 = 202
+
+print(f'{int1 == int2 = }')
+print(f'{id(int1) == id(int1) = }')
+print(f'{int1 is int2 = }\n')
+
+
+list1 = [1, 2, 3, 4]
+list2 = [1, 2, 3, 4]
+
+print(f'{list1 == list2 = }')
+print(f'{id(list1) == id(list2) = }')
+print(f'{list1 is list2 = }\n')
+
---
+++ base/operator_math.py
@@ -0,0 +1,32 @@
+# унарные операторы
+print( +2 )
+print( -3 )
+
+# бинарные операторы
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3)
+print(2 // 3)
+print(2 % 3)
+print(2 ** 3, end='\n\n')
+
+# расширенные операторы присваивания
+num = 25
+
+num += 5
+# num = num + 5
+print(num)
+
+num -= 5
+print(num)
+num *= 5
+print(num)
+num /= 5
+print(num)
+num //= 5
+print(num)
+num %= 3
+print(num)
+num **= 7
+print(num)
