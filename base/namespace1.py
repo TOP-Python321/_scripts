cd44271ff9707e44c660c2a53431f975ad3c5b27
Gennadiy <renardes@gmail.com>
2023-05-27 13:16:27 +0500

    словарные представления пространств имён

---
+++ base/namespace1.py
@@ -0,0 +1,7 @@
+# независимые пространства имён
+namespace1 = {'var1': 0912378645, 'var2': 124987346, 'var3': 29384587346}
+namespace2 = {'var1': 0891238742, 'var2': 187342455}
+
+# вложенные пространства имён
+local_namespace = {'arg1': ..., 'arg2': ..., 'a': ...}
+global_namespace = {'a': 'variable a', 'main': local_namespace, 'test_str': 'user input'}
