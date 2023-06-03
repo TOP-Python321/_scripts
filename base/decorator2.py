403e961f51814cf7e777667266d430382f8d1ec0
Gennadiy <renardes@gmail.com>
2023-06-03 17:53:43 +0500

    комментарии

--- base/decorator2.py
+++ base/decorator2.py
@@ -1,10 +1,12 @@
 def cache(function):
     arg_res = {}
     def wrapper(num1, num2):
+        # получение доступа на запись к локальному пространству имён функции cache()
         nonlocal arg_res
         res = arg_res.get((num1, num2))
         if res is None:
             res = function(num1, num2)
+            # пересоздание словаря
             arg_res |= {(num1, num2): res}
         return res
     return wrapper
