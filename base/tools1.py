b2a01bf63fc2de73bac5f1739d11271a678dff57
Gennadiy <renardes@gmail.com>
2023-06-04 14:35:23 +0500

    примеры использования функций из модулей itertools и functools

---
+++ base/tools1.py
@@ -0,0 +1,29 @@
+from itertools import repeat, chain
+from functools import reduce
+from pprint import pprint
+
+
+class Test:
+    pass
+
+
+repeater_manaul = [Test()]*3
+repeater = repeat(Test(), 3)
+
+
+print(*(id(obj) for obj in repeater_manaul), sep='\n', end='\n\n')
+print(*(id(obj) for obj in repeater), sep='\n', end='\n\n')
+
+
+# codes = tuple(range(48, 58)) + tuple(range(65, 91))
+codes = chain(range(48, 58), range(65, 91))
+digits = {chr(code): i for i, code in enumerate(codes)}
+
+pprint(digits)
+print()
+
+
+numbers = [2, 9, 3, 4, 7, 9, 2, 5]
+print(reduce(lambda x, y: x*y, numbers), end='\n\n')
+
+
