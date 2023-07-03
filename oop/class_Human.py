9aacf60b7f68b065d215bfec7a2180dc62804de6
Gennadiy <renardes@gmail.com>
2023-07-03 10:03:40 +0500

    обычные и статические методы

--- oop/class_Human.py
+++ oop/class_Human.py
@@ -5,9 +5,21 @@ class Human:
     legs = 2
     toes = 5
     
-    def speak(self, message: str):
-        print(self)
+    def check_id(self):
+        print(id(self))
+    
+    def rename(self, new_name: str) -> None:
+        self.name = new_name
+    
+    @staticmethod
+    def speak(message: str) -> None:
         print(message)
+    
+    def __repr__(self):
+        return f'<Human: name={self.name}>'
+    
+    def __str__(self):
+        return f'Привет, меня зовут {self.name}!'
 
 
 # атрибуты объекта класса не считая встроенных (начинаются и заканчиваются на '__')
@@ -38,4 +50,51 @@ for person in (igor, ilya, inna):
 # <bound method Human.speak of <__main__.Human object at 0x0000018FF12F8090>>
 
 # при вызове метода происходит вызов функции от объекта класса с передачей экземпляра в качестве первого позиционного аргумента
-# igor.speak('привет') -> Human.speak(igor, 'привет')
+# igor.check_id() -> Human.check_id(igor)
+# igor.rename('ИГОР') -> Human.rename(igor, 'ИГОР')
+
+
+# >>> id(ilya)
+# 2469153017680
+
+# >>> ilya.check_id()
+# 2469153017680
+
+
+# >>> Human.speak
+# <function Human.speak at 0x000001B6EEB9FA60>
+
+# >>> inna.speak
+# <function Human.speak at 0x000001B6EEB9FA60>
+
+# >>> inna.speak('привет')
+# привет
+
+# >>> Human.speak('привет')
+# привет
+
+
+# >>> igor
+# <Human: name=Игорь>
+
+# >>> f'{igor!r}'
+# '<Human: name=Игорь>'
+
+# >>> repr(igor)
+# '<Human: name=Игорь>'
+
+# >>> igor.__repr__()
+# '<Human: name=Игорь>'
+
+
+# >>> print(igor)
+# Привет, меня зовут Игорь!
+
+# >>> f'{igor!s}'
+# 'Привет, меня зовут Игорь!'
+
+# >>> str(igor)
+# 'Привет, меня зовут Игорь!
+
+# >>> igor.__str__()
+# 'Привет, меня зовут Игорь!'
