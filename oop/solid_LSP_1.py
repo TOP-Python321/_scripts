74000888040a752cee4c053ee5bdd5979d283a7d
Gennadiy <renardes@gmail.com>
2023-08-21 00:24:06 +0500

    принцип подстановки Лисков — встроенные классы

---
+++ oop/solid_LSP_1.py
@@ -0,0 +1,23 @@
+"""Liskov Substitution Principle — Принцип Подстановки Лисков"""
+
+
+# >>> int
+# <class 'int'>
+# >>> bool
+# <class 'bool'>
+# >>>
+# >>> bool.__mro__
+# (<class 'bool'>, <class 'int'>, <class 'object'>)
+# >>>
+# >>> int(True)
+# 1
+# >>> int(False)
+# 0
+# >>>
+# >>> nums = [1, 2, 3, 4]
+# >>>
+# >>> nums[1]
+# 2
+# >>> nums[True]
+# 2
+
