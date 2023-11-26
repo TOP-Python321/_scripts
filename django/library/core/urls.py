bea69b63ae918b68720242144b42ad51e8d4f5a7
Gennadiy <renardes@gmail.com>
2023-11-26 15:15:21 +0500

    добавление статических файлов

--- django/library/core/urls.py
+++ django/library/core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 
 # from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog import views
@@ -36,5 +38,7 @@ urlpatterns = [
     
     path('test', views.test),
     path('test_sub', views.test_sub),
-]
+] 
+
+urlpatterns += static(settings.STATIC_URL)
 
